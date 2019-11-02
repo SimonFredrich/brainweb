54174d3b608d1b1e46c24d8f112c1f0202fd7909
SimonFredrich <21simplex02@gmail.com>
2019-11-02 17:36:38 +0100

    Updates

---
+++ precache-manifest.6b35fd3c8a951f4fb5f451a52b57c183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963e2aa2174bec4a4e0b281da4b3457e",
+    "url": "/brainweb/index.html"
+  },
+  {
+    "revision": "00d7633c129a7836735b",
+    "url": "/brainweb/static/css/main.aecd81a9.chunk.css"
+  },
+  {
+    "revision": "cc97cfb916b39a8608e7",
+    "url": "/brainweb/static/js/2.a0f9ce5f.chunk.js"
+  },
+  {
+    "revision": "00d7633c129a7836735b",
+    "url": "/brainweb/static/js/main.1fb8f111.chunk.js"
+  },
+  {
+    "revision": "5f3d5f4c342e25cb504a",
+    "url": "/brainweb/static/js/runtime-main.c5806170.js"
+  }
+]);
