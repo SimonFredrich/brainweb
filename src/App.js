3abddd20fa099b60f6b1b9bf12b2c24a4a39e9d1
SimonFredrich <21simplex02@gmail.com>
2019-11-02 19:41:56 +0100

    test content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Paragraph from './components/paragraph';
+import data from './database/geschichte.json';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
             <header className="App-header">
                 <h1>BrainWeb</h1>
             </header>
-            <Paragraph content='Simon'/>
+            <Paragraph content={data.themen.attische_demokratie.unterthema1.content}/>
         </div>
     );
 }
